b1cf16840c44572c055877380c24f7e072589642
gooostaw <gooostaw@gmail.com>
2025-03-01 18:01:00 +0100

    set SERIAL_FLOAT_PRECISION = 4

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2477,7 +2477,7 @@
 #define SERIAL_OVERRUN_PROTECTION
 
 // For serial echo, the number of digits after the decimal point
-//#define SERIAL_FLOAT_PRECISION 4
+#define SERIAL_FLOAT_PRECISION 4
 
 /**
  * Set the number of proportional font spaces required to fill up a typical character space.
